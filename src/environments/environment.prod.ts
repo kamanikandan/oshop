e6cc6aaae4e1d1923c91c524b95a00b5f7a37d65
Manikandan Kaliyappan <MK00547145@techmahindra.com>
2019-05-03 12:04:06 +0530

    Development environment setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBWqe4ZKv9sxDJub0nNLOYZyK7uPTd04pI",
+    authDomain: "oshop-8e129.firebaseapp.com",
+    databaseURL: "https://oshop-8e129.firebaseio.com",
+    projectId: "oshop-8e129",
+    storageBucket: "oshop-8e129.appspot.com",
+    messagingSenderId: "838911652263"
+  }
+};
